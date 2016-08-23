225946e5deaee9d289a547ceeab5dded1d300bf3
Carter Bailey <carter.l.bailey@gmail.com>
2016-08-22 21:18:20 -0500

    added change action and custom route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  resources :tasks
+  resources :tasks do
+    member do
+      put :change
+    end
+  end
   devise_for :users
   get 'pages/home'
 
   root 'pages#home'
 
   get 'about' => 'pages#about'
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
