5813b60444946de8bfa57411f939228d735708b6
Carter Bailey <carter.l.bailey@gmail.com>
2016-08-22 21:33:45 -0500

    added test page to play with bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,11 @@ Rails.application.routes.draw do
     end
   end
   devise_for :users
-  get 'pages/home'
 
   root 'pages#home'
 
+  get 'test' => 'pages#test'
+  get 'pages/home'
   get 'about' => 'pages#about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
